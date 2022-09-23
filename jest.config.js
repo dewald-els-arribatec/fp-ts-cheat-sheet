59253ecb1bbf700b79ed26e6bcf812153765a0e2
kahin <kahin.lau@arribatec.com>
2022-09-23 11:07:50 +0200

    Init

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: 'node',
+    verbose: true,
+    modulePathIgnorePatterns: ['dist'],
+    coverageDirectory: './coverage/',
+    coverageReporters: ["json", "lcov", "text", "clover", "cobertura"]
+};
---
+++ src/index.test.ts
@@ -0,0 +1,3 @@
+describe('testing', () => {
+    it("should pass", () => expect(1).toStrictEqual(1));
+});
